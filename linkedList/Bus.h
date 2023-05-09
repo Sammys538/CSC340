d67ee5b2e448dd0ad36a7fea69a3b4f7c616862e
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-09 01:14:01 -0700

    Update Bus.h

--- linkedList/Bus.h
+++ linkedList/Bus.h
@@ -5,17 +5,11 @@
 // Creates Bus Class
 class Bus{
 	public:
-	
 		int GetCapacity() const;
-		
 		string GetBusNumber() const;
-		
 		bool isFull() const;
-		
 		Bus();
-		
 		void Print();
-		
 		void dropOff();
 		void pickUp();
 	private:
