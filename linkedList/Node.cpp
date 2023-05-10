27b731c3e76f7628bacd71ba9cc63c288892a6e6
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-10 15:19:53 -0700

    Added Node class files

---
+++ linkedList/Node.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "Node.h"
+
+
+Node::Node(){
+	data = "node";
+	prevNode = nullptr;
+	nextNode = nullptr;
+	currNode = nullptr;
+	headNode = nullptr;
+}
+
+string Node::GetData(){
+	return data;
+}
+
