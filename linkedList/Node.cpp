89704298ff6b4985c677eabd9079b522495f0012
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-15 14:37:34 -0700

    Update Node.cpp

--- linkedList/Node.cpp
+++ linkedList/Node.cpp
@@ -1,19 +1,35 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// #include "Node.h"
+#include "Node.h"
 
-// Node::Node(){
-// 	data = "node";
-// 	prevNode = nullptr;
-// 	nextNode = nullptr;
-// 	currNode = nullptr;
-// 	headNode = nullptr;
-// }
 
-// string Node::GetData(){
-// 	return data;
-// }
+Node::Node(){
+	data = "node";
+	prevNode = nullptr;
+	nextNode = nullptr;
+}
 
- 
- 
+string Node::GetData(){
+	return data;
+}
+
+Node* Node::GetNextNode(){
+	return nextNode;
+}
+
+Node* Node::GetPrevNode(){
+	return prevNode;
+}
+
+void Node::SetData(string input){
+	data = input;
+}
+
+void Node::SetNextNode(Node* next){
+	nextNode = next;
+}
+
+void Node::SetPrevNode(Node* prev){
+	prevNode = prev;
+}
