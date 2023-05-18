3e99dd6d82a5d0ce9e5c502a9cf6e06a49ca7bd7
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-17 21:17:06 -0700

    Update Link.h

--- linkedList/Link.h
+++ linkedList/Link.h
@@ -1,15 +1,31 @@
 
-//#include "List.h"
+// //#include "List.h"
+// #ifndef LINK_H
+// #define LINK_H
+
+// class Link: public Node{
+
+//     public:
+//     Link(Node* Head, Node* Curr, Node* Temp);
+//     Link();
+//     virtual void makeList() = 0;
+    
+// };
+
+// #endif
+
 #ifndef LINK_H
 #define LINK_H
 
-class Link: public Node{
+class Link{
 
     public:
-    Link(Node* Head, Node* Curr, Node* Temp);
-    Link();
-    virtual void makeList() = 0;
-    
+ //    Link(Node* Head, Node* Curr, Node* Temp);
+   //  Link();
+   //  virtual void makeList() = 0; // comment out
+    	virtual void report() = 0;
+    	
+//     
 };
 
 #endif
