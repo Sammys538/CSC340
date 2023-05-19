9bd6494bee6c5193e7d912d3171c6205dc24babc
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-19 06:04:25 -0700

    Update People.h

--- linkedList/People.h
+++ linkedList/People.h
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include "Link.h"
@@ -7,23 +6,27 @@ using namespace std;
 #ifndef PEOPLE_H
 #define PEOPLE_H
 
+
+//Class that creates People class, which contain ID, name and age
 class People: public Link{
 
-   public:
-        string ID;
+	protected:
+        int ID;
         string name;
         int age;
 
-    People(Node* Head, Node* Curr, Node* Temp, string id, string Name, int Age);
-    People();
-    void setID(string id);
-    void setName(string Name);
-    void setAge(int Age);
-    string getID();
-    string getName();
-    int getAge();
-    void print();
-    void makeList();
+	public:
+   		People(int id, string Name, int Age);
+    	People();
+    	void setID(int id);
+    	void setName(string Name);
+    	void setAge(int Age);
+    	int getID();
+    	string getName();
+    	int getAge();
+    	void print();
+    	void report();
+    	int rideAmount();
 };
 
 #endif
