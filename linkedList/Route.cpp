e4ce2846f0632e4743705302cc7c43859d3b4d63
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-19 06:05:38 -0700

    Update Route.cpp

--- linkedList/Route.cpp
+++ linkedList/Route.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
 #include "Route.h"
 
@@ -17,7 +18,26 @@ Route::Route(){
 	schedule = "1 2 3 4";
 }
 
+Route::Route(string input){
+	schedule = input;
+}
+
+void updateSchedule(string schedule){
+	string str = schedule.substr(2, (schedule.size()-1));
+	schedule = str;
+}
+
 void Route::Print(){
 	cout << schedule << endl;
 }
 
+void Route::report(){
+	cout << endl;
+	cout << "ROUTE" << endl;
+	cout << "Schedule: " << schedule << endl;
+	cout << endl;
+}
+
+int Route::rideAmount(){
+	return 1;
+}
