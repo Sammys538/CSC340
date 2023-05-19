0a4f7bdc32c2d684cbb2f25b5f1e2a64859a8704
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-19 06:06:44 -0700

    Update Stops.h

--- linkedList/Stops.h
+++ linkedList/Stops.h
@@ -3,14 +3,17 @@
 #define Stops_H
 
 // Creates Stops Class
-class Stops{
+class Stops: public Link{
 	public:
 		int GetAmount();
 		int GetLocation();
 		void SetAmount(int input);
 		void SetLocation(int input);
 		Stops();
+		Stops(int riders, int stopNum);
 		void Print();
+		void report();
+		int rideAmount();
 	private:
 		int amount;
 		int location;
