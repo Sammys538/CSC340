32ae753d06639aed48d8c7a4f17842d55c355dfd
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-19 05:59:56 -0700

    Update Bus.h

--- linkedList/Bus.h
+++ linkedList/Bus.h
@@ -1,24 +1,27 @@
 #include <iostream>
 #ifndef Bus_H
 #define Bus_H
+#include "Link.h"
 
 // Creates Bus Class
-class Bus{
+class Bus: public Link{
 	public:
 		int GetCapacity() const;
-		string GetBusNumber() const;
+		int GetBusNumber() const;
 		int GetPassengers() const;
 		void SetCapacity(int input);
 		void SetPassengers(int input);
+		void SetBusNumber(int input);
 		bool isFull() const;
 		Bus();
+		Bus(int cap, int busNum, int pass);
 		void Print();
 		void DropOff();
 		void PickUp();
 		void report();
-	
+		int rideAmount();
 	private:
-		string busNumber;
+		int busNumber;
 		int capacity;
 		int passengers;
 };
