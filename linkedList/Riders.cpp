44fa7318007720b7363fcad442d3adb838190599
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-19 06:04:53 -0700

    Update Riders.cpp

--- linkedList/Riders.cpp
+++ linkedList/Riders.cpp
@@ -2,25 +2,27 @@
 #include "Riders.h"
 using namespace std;
 
-    Riders::Riders(Node* Head, Node* Curr, Node* Temp, string id, string Name, int Age, double Fare, int Destination, int CurrStop):People(Head, Curr, Temp, id, Name, Age){
-                fare = Fare;
-                destination = Destination;
-                currStop = CurrStop;
+    Riders::Riders(int id, string Name, int Age, double Fare, int Destination, int CurrStop):People(id, Name, Age){
+            fare = Fare;
+            destination = Destination;
+            currStop = CurrStop;
     }
-
+    
     Riders::Riders():People(){
     	fare = 2.5;
     	destination = 3;
-    	currStop = 0;
+    	currStop = 1;
     
     }
-
+    
     void Riders::setCurrStop(int CurrStop){
         currStop = CurrStop;
     }
+    
     int Riders::getCurrStop(){
         return currStop;
     }
+    
     void Riders::setFare(double Fare){
         fare = Fare;
     }
@@ -36,12 +38,19 @@ using namespace std;
     double Riders::getFare(){
         return fare;
     }
+    
     void Riders::print(){
-        cout <<  "Name: " << getName() << ", ID: "  << getID() << ", Age:" << getAge() << ", Current Stop: " 
-             << getCurrStop() << ", Destination: " << getDestination() << endl;
+        // cout <<  "Name: " << getName() << ", ID: "  << getID() << ", Age: " << getAge() << ", Current Stop: " 
+//              << getCurrStop() << ", Destination: " << getDestination() <<endl;
+		cout << "Name: " << name << ", ID: " << ID << ", Age: " << age <<
+    		", Destination: " << destination << ", Current Stop: " << currStop << endl;
     }
-    void Riders::makeList(){
-        cout << "Makes  Riders list." << endl;
-        cout << endl;
-        
+    
+    void Riders::report(){
+    	cout << "Name: " << name << ", ID: " << ID << ", Age: " << age << ", Fare: $" << fare << ""
+    	", Current Stop: " << currStop << ", Destination: " << destination << endl;
+    }
+    
+    int Riders::rideAmount(){
+    	return destination;
     }
