800162069284cd42f007de4e0c9c2fa06b193519
Sammy Rodriguez <86125731+Sammys538@users.noreply.github.com>
2023-05-19 20:13:38 -0700

    Update Link.h

--- linkedList/Link.h
+++ linkedList/Link.h
@@ -1,30 +1,14 @@
-
-// //#include "List.h"
-// #ifndef LINK_H
-// #define LINK_H
-
-// class Link: public Node{
-
-//     public:
-//     Link(Node* Head, Node* Curr, Node* Temp);
-//     Link();
-//     virtual void makeList() = 0;
-    
-// };
-
-// #endif
-
+// Group Members: Sammy Rodriguez, Jared Barbero, and Pablo Partida
 #ifndef LINK_H
 #define LINK_H
 
 class Link{
 
     public:
- //    Link(Node* Head, Node* Curr, Node* Temp);
-   //  Link();
-   //  virtual void makeList() = 0; // comment out
-    	virtual void report() = 0;
-        virtual int rideAmount() = 0;
+    	virtual void report() = 0; // To print out information of class
+    	virtual int rideAmount() = 0; // Used to get amount of riders at each station/stop
+    	virtual void boardPass(int input) = 0; // Used to increment passengers in Bus
+    	
     	
 //     
 };
